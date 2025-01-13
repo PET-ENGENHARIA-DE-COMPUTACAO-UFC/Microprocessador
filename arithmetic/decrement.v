9c68ccc62bfcffbcf183073600b0201a063469d8
Gabriel Amorim <137211057+gabrielmelo7@users.noreply.github.com>
2025-01-13 18:41:20 -0300

    Adicionando cout ao módulo decrementador

--- arithmetic/decrement.v
+++ arithmetic/decrement.v
@@ -1,7 +1,6 @@
 `timescale 1ps/1ps
 `include "subtractor.v"
 
-module decrement8b(output wire[7:0] result, input wire[7:0] num1);
-  wire cout;
+module decrement8b(output wire[7:0] result, output wire cout, input wire[7:0] num1);
   full_subtractor8b FULL_SUBTRACTOR8bDEC(.csub(result), .c_outc(cout), .numf1(num1), .numf2(8'b00000001));
 endmodule
